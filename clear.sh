1742e624d777fb422bb3c07cefaeb35eccead5e8
pulshen <pulshencode@outlook.com>
2016-03-01 17:36:24 +0200

    Fixed PATH to build_dir

--- clear.sh
+++ clear.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd build_dir
 VAR=$(ls | grep _wrt)
 echo Removing there dirs : $VAR
 rm -rf $VAR
 echo Cleaning tmp dir
-rm -rf tmp/
+rm -rf ../tmp/
--- pwrt_setup.sh
+++ pwrt_setup.sh
@@ -8,13 +8,13 @@ echo -n "Choose an action: "
 read item
 case "$item" in
   1) echo "Cloning repository"
-  cd tmp && git clone https://github.com/Sudokamikaze/PulshenWRT_BB.git &&  echo Applying patches && cp -r PulshenWRT_BB/files ../bb_wrt/ && cd ../bb_wrt && patch < ../tmp/PulshenWRT_BB/PulshenWRT_BB.diff
+  cd tmp && git clone https://github.com/Sudokamikaze/PulshenWRT_BB.git &&  echo Applying patches && cp -r PulshenWRT_BB/files ../build_dir/bb_wrt/ && cd ../build_dir/bb_wrt && patch < ../../tmp/PulshenWRT_BB/PulshenWRT_BB.diff
   ;;
   2) echo "Cloning repository"
-  cd tmp && git clone https://github.com/Sudokamikaze/PulshenWRT_CC.git &&  echo Applying patches && cp -r PulshenWRT_CC/files ../cc_wrt/ && cd ../cc_wrt && patch < ../tmp/PulshenWRT_CC/PulshenWRT_CC.diff
+  cd tmp && git clone https://github.com/Sudokamikaze/PulshenWRT_CC.git &&  echo Applying patches && cp -r PulshenWRT_CC/files ../build_dir/cc_wrt/ && cd ../build_dir/cc_wrt && patch < ../../tmp/PulshenWRT_CC/PulshenWRT_CC.diff
   ;;
   3) echo "Cloning repository"
-  cd tmp && git clone https://github.com/Sudokamikaze/PulshenWRT_trunk.git &&  echo Applying patches && cp -r PulshenWRT_trunk/files ../trunk_wrt/ && cd ../trunk_wrt && patch < ../tmp/PulshenWRT_trunk/PulshenWRT_trunk.diff
+  cd tmp && git clone https://github.com/Sudokamikaze/PulshenWRT_trunk.git &&  echo Applying patches && cp -r PulshenWRT_trunk/files ../build_dir/trunk_wrt/ && cd ../build_dir/trunk_wrt && patch < ../../tmp/PulshenWRT_trunk/PulshenWRT_trunk.diff
   ;;
   *) echo "Waiting for input"
   ;;
@@ -29,16 +29,16 @@ echo -n "Choose an action: "
 read uboot
 case "$uboot" in
   1) echo "Unlocking for BB"
-  cd ../bb_wrt/target/linux/ar71xx/files/drivers/mtd/
-  patch < ../../../../../../../tmp/PulshenWRT_BB/uboot_unlock.patch
+  cd ../build_dir/bb_wrt/target/linux/ar71xx/files/drivers/mtd/
+  patch < ../../../../../../../../tmp/PulshenWRT_BB/uboot_unlock.patch
   ;;
   2) echo "Unlocking for CC"
-  cd ../cc_wrt/target/linux/ar71xx/files/drivers/mtd/
-  patch < ../../../../../../../tmp/PulshenWRT_CC/uboot_unlock.patch
+  cd ../build_dir/cc_wrt/target/linux/ar71xx/files/drivers/mtd/
+  patch < ../../../../../../../../tmp/PulshenWRT_CC/uboot_unlock.patch
   ;;
   3) echo "Unlocking for trunk"
-  cd ../trunk_wrt/target/linux/ar71xx/files/drivers/mtd/
-  patch < ../../../../../../../tmp/PulshenWRT_trunk/uboot_unlock.patch
+  cd ../build_dir/trunk_wrt/target/linux/ar71xx/files/drivers/mtd/
+  patch < ../../../../../../../../tmp/PulshenWRT_trunk/uboot_unlock.patch
   ;;
   *) echo "Waiiting for input"
   ;;
