41c6bc73d27381d0f240c520ea5c6c861abd080a
sudokamikaze <pulshencode@outlook.com>
2016-05-17 21:42:24 +0300

    Rewrited all process to setup to all-in-one script

--- device_selector.sh
+++ device_selector.sh
@@ -2,7 +2,15 @@
 echo -n "Install libs for building? Y/n: "
 read libs
 case "$libs" in
-  y|Y) ./bin/prepare_utils.sh
+  y|Y) echo "Installing..."
+  OS=$(cat /etc/*-release | grep ID=)
+  echo $OS
+  if [ $OS == ID=arch ]; then sudo pacman -S --needed subversion asciidoc bash bc binutils bzip2 fastjar flex git gcc util-linux gawk intltool zlib make cdrkit ncurses openssl patch perl-extutils-makemaker rsync sdcc unzip wget gettext libxslt boost libusb bin86 sharutils b43-fwcutter findutils
+  elif [ $OS == ID=ubuntu ]; then sudo apt-get install subversion git g++ libncurses5-dev zlib1g-dev gawk libssl-dev
+  else
+   echo Unsuported distro
+  fi
+  echo Done!
   ;;
   n|N)
   ;;
@@ -16,8 +24,6 @@ echo -n "Choose the device: "
 read item
 case "$item" in
   1) echo "Starting the setup process for TL-WR841N"
-  ./bin/prepare_sources.sh
-  ./bin/pwrt_setup.sh
   ;;
   2) echo "Starting the setup process for x86"
   cd build_dir
@@ -28,4 +34,34 @@ case "$item" in
   *) echo "Nothing entered"
   ;;
 esac
-echo Done!
+echo ========================
+echo 1 Barrier Breaker
+echo 2 Chaos Calmer
+echo 3 Trunk
+echo ========================
+echo -n "Choose version: "
+read sources
+case "$sources" in
+  1) echo "Cloning repository"
+  cd build_dir
+  git clone git://git.openwrt.org/14.07/openwrt.git bb_wrt && cd bb_wrt
+  ./scripts/feeds update -a && ./scripts/feeds install -a
+  cp ../../configs_default/config_bb ./.config
+  ;;
+  2) echo "Cloning repository"
+  cd build_dir
+  git clone git://git.openwrt.org/15.05/openwrt.git cc_wrt && cd cc_wrt
+  ./scripts/feeds update -a && ./scripts/feeds install -a
+  cp ../../configs_default/config_cc ./.config
+  ;;
+  3) echo "Cloning repository"
+  cd build_dir
+  git clone git://git.openwrt.org/openwrt.git trunk_wrt && cd trunk_wrt
+  ./scripts/feeds update -a && ./scripts/feeds install -a
+  cp ../../configs_default/config_trunk ./.config
+  ;;
+  *) echo "Waiting for input"
+  ;;
+esac
+echo "Starting PWRT setup script..."
+./pwrt_setup.sh
