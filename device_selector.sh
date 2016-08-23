40035ff6c8738189eec9a0d03d0d12d7529a0692
Sudokamikaze <pulshencode@outlook.com>
2016-08-23 16:46:59 +0300

    Removed gcc from install libs and added gcc-multilib

--- device_selector.sh
+++ device_selector.sh
@@ -5,7 +5,7 @@ case "$libs" in
   y|Y) echo "Installing..."
   OS=$(cat /etc/*-release | grep ID=)
   echo $OS
-  if [ $OS == ID=arch ]; then sudo pacman -S --needed subversion asciidoc bash bc binutils bzip2 fastjar flex git gcc util-linux gawk intltool zlib make cdrkit ncurses openssl patch perl-extutils-makemaker rsync sdcc unzip wget gettext libxslt boost libusb bin86 sharutils b43-fwcutter findutils
+  if [ $OS == ID=arch ]; then sudo pacman -S --needed subversion asciidoc bash bc binutils bzip2 fastjar flex git gcc-multilib util-linux gawk intltool zlib make cdrkit ncurses openssl patch perl-extutils-makemaker rsync sdcc unzip wget gettext libxslt boost libusb bin86 sharutils b43-fwcutter findutils
   elif [ $OS == ID=ubuntu ]; then sudo apt-get install subversion git g++ libncurses5-dev zlib1g-dev gawk libssl-dev
   else
    echo Unsuported distro
