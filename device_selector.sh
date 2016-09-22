060f3bd40a4a4d2780cafd7df221c3de8da7e8f7
Sudokamikaze <pulshencode@outlook.com>
2016-09-22 20:54:44 +0300

    Droped BB Support

--- device_selector.sh
+++ device_selector.sh
@@ -35,34 +35,27 @@ case "$item" in
   ;;
 esac
 echo ========Openwrt=========
-echo "1. Barrier Breaker"
-echo "2. Chaos Calmer"
-echo "3. Trunk"
+echo "1. Chaos Calmer"
+echo "2. Trunk"
 echo =========LEDE===========
-echo "4. Latest trunk"
+echo "3. Latest trunk"
 echo ========================
 echo -n "Choose version: "
 read sources
 case "$sources" in
   1) echo "Cloning repo"
   cd build_dir
-  git clone git://git.openwrt.org/14.07/openwrt.git bb_wrt && cd bb_wrt
-  ./scripts/feeds update -a && ./scripts/feeds install -a
-  cp ../../configs_default/config_bb ./.config
-  ;;
-  2) echo "Cloning repo"
-  cd build_dir
   git clone git://git.openwrt.org/15.05/openwrt.git cc_wrt && cd cc_wrt
   ./scripts/feeds update -a && ./scripts/feeds install -a
   cp ../../configs_default/config_cc ./.config
   ;;
-  3) echo "Cloning repo"
+  2) echo "Cloning repo"
   cd build_dir
   git clone git://git.openwrt.org/openwrt.git trunk_wrt && cd trunk_wrt
   ./scripts/feeds update -a && ./scripts/feeds install -a
   cp ../../configs_default/config_trunk ./.config
   ;;
-  4) echo "Clonning repo"
+  3) echo "Clonning repo"
   cd build_dir
   git clone git@github.com:lede-project/source.git trunk_lede && cd trunk_lede
   ./scripts/feeds update -a && ./scripts/feeds install -a
