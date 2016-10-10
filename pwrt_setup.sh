1c437cf2ef20e46ae8631c607091ae882e31785c
Sudokamikaze <pulshencode@outlook.com>
2016-10-10 17:39:35 +0300

    Rewrite all to functions

--- pwrt_setup.sh
+++ pwrt_setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+manuallaunch=true
+
 figlet PulshenWRT
 
 function callpwrt {
@@ -18,59 +21,70 @@ function calluboot {
   patch < ../../../../../../../../tmp/$imlovewrt/uboot_unlock.patch
 }
 
-echo ======================================
-echo "1. PulshenWRT stable (OPENWRT 15.05.1)"
-echo "2. PulshenWRT upstream (OPENWRT Trunk)"
-echo "3. PulshenWRT testing (LEDE TRUNK)"
-echo ======================================
-echo -n "Select version: "
-read item
-case "$item" in
-  1) echo "Cloning repo"
-  imlovewrt=PulshenWRT_CC
-  morelove=cc_wrt
-  callpwrt
+function definevars {
+  case "$item" in
+    1) echo "Cloning repo"
+    imlovewrt=PulshenWRT_CC
+    morelove=cc_wrt
+    callpwrt
+    ;;
+    2) echo "Cloning repo"
+    imlovewrt=PulshenWRT_trunk
+    morelove=trunk_wrt
+    callpwrt
+    ;;
+    3) echo "Cloning repo"
+    imlovewrt=PulshenWRT_LEDE
+    morelove=trunk_lede
+    callpwrt
+    ;;
+    *) echo "Waiting for input"
+    ;;
+  esac
+}
+
+
+function displaymenu {
+  echo ======================================
+  echo "1. PulshenWRT stable (OPENWRT 15.05.1)"
+  echo "2. PulshenWRT upstream (OPENWRT Trunk)"
+  echo "3. PulshenWRT testing (LEDE TRUNK)"
+  echo ======================================
+  echo -n "Select version: "
+  read item
+  definevars
+}
+
+case "$manuallaunch" in
+  false) echo Automatic install for $dirwrt
+  eval $(grep dirwrt= ./device_selector.sh)
+  if [ $dirwrt == cc_wrt ]; then
+  item=1
+  elif [ $dirwrt == trunk_wrt ]; then
+  item=2
+  elif [ $dirwrt == trunk_lede ]; then
+  item=3
+  fi
+  definevars
   ;;
-  2) echo "Cloning repo"
-  imlovewrt=PulshenWRT_trunk
-  morelove=trunk_wrt
-  callpwrt
-  ;;
-  3) echo "Cloning repo"
-  imlovewrt=PulshenWRT_LEDE
-  morelove=trunk_lede
-  callpwrt
-  ;;
-  *) echo "Waiting for input"
+  true) displaymenu
   ;;
 esac
 echo -n "Do you need U-Boot Unlock? "
 read ubootcheck
 if [ $ubootcheck == y ]; then
-echo "OK, that version of PulshenWRT you have?"
+echo "Unlock.."
 elif [ $ubootcheck == Y ]; then
-echo "OK, that version of PulshenWRT you have?"
+echo "Unlock.."
 else
   echo " "
 fi
-echo ======================================
-echo 1 Unlock u-boot for PulshenWRT CC
-echo 2 Unlock u-boot for PulshenWRT TRUNK
-echo 3 Unlock u-boot for PulshenWRT LEDE
-echo ======================================
-echo -n "Choose an action: "
-read uboot
-case "$uboot" in
-  1) echo "Unlocking for CC"
-  calluboot
+case "$morelove" in
+  cc_wrt) calluboot
   ;;
-  2) echo "Unlocking for TRUNK"
-  calluboot
+  trunk_wrt) calluboot
   ;;
-  3) echo "Unlocking for LEDE"
-  calluboot
-  ;;
-  *) echo "Waiiting for input"
+  trunk_lede) calluboot
   ;;
 esac
 echo Done
